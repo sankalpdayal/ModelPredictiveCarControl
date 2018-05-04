cdebcf3d43b2949746b6807cd8670e280fe00f88
Sankalp Dayal <sadayal@invensense.com>
2018-05-04 12:17:37 -0700

    Increasing ref vel to 50

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -22,7 +22,7 @@ double dt = 0.05;
 const double Lf = 2.67;
 
 // The reference velocity is set to 40 mph.
-double ref_v = 40;
+double ref_v = 50;
 
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should to establish
