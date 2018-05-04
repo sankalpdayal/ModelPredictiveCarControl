e69c4cffc7e0579d885d3ad3bb3cf7a0462c3e56
Sankalp Dayal <sadayal@invensense.com>
2018-05-04 13:10:10 -0700

    Changing weights to keep high speed

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,7 +6,7 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 25;
+size_t N = 20;
 double dt = 0.05;
 
 // This value assumes the model presented in the classroom is used.
@@ -22,7 +22,7 @@ double dt = 0.05;
 const double Lf = 2.67;
 
 // The reference velocity is set to 40 mph.
-double ref_v = 45;
+double ref_v = 40;
 
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should to establish
